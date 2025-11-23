08647ed0d2dce912ccbfffad1035cadb4c014f51
Kaitlyn <kek.krause03@gmail.com>
2025-11-23 12:09:59 -0500

    bruhmorning

--- display.php
+++ display.php
@@ -30,11 +30,54 @@
             <table>
                 <caption>Displaying <?php print("$select") ?> Information</caption>
                 <?php
-                    while($row = mysqli_fetch_row($result)){
-                        print("<tr>");
-                        foreach($row as $value)
-                            print("<td>$value</td>");
-                        print("</tr>");
+                    if($select === "Listings"){
+                        $houses = array();
+                        $businesses = array();
+                        $database = mysqli_connect( "localhost", "root", "" );
+                        mysqli_select_db($database, "Real_Estate");
+                        while($row = mysqli_fetch_array($result)){
+                            $address = $row[1];
+                            $query2 = "SELECT * FROM House WHERE address = \"" . $address . "\"";
+                            $result2 = mysqli_query( $database, $query2 );
+                            if($result2->num_rows === 0){
+                                $businesses[] = $row;
+                            }
+                            else{
+                                $houses[] = $row;
+                            }
+                        }
+                        mysqli_close($database);
+                        print("<table>");
+                        print("<caption>House Listings</caption>");
+                        foreach($houses as $house){
+                            print("<tr>");
+                            foreach($house as $value){
+                                print("<td>$value</td>");
+                            }
+                            print("<tr>");
+                        }
+                        print("</table>");
+
+                        print("<br>");
+
+                        print("<table>");
+                        print("<caption>Business Property Listings</caption>");
+                        foreach($businesses as $property){
+                            print("<tr>");
+                            foreach($property as $value){
+                                print("<td>$value</td>");
+                            }
+                            print("<tr>");
+                        }
+                        print("</table>");
+                    }
+                    else{
+                        while($row = mysqli_fetch_row($result)){
+                            print("<tr>");
+                            foreach($row as $value)
+                                print("<td>$value</td>");
+                            print("</tr>");
+                        }
                     }
                 ?>
             </table>
